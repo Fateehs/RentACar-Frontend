dcac18e1c609724c12f0987faa9847899cfbda75
Fateehs <benzer1010@gmail.com>
2022-11-05 14:23:48 +0300

    User Child Base added

---
+++ src/app/components/user/bases/user-child-component-base/user-child-component-base.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonChildComponentBaseComponent } from 'src/app/components/public/bases/common-child-component-base/common-child-component-base.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+@Component({
+  selector: 'app-user-child-component-base',
+  templateUrl: './user-child-component-base.component.html',
+  styleUrls: ['./user-child-component-base.component.css']
+})
+export class UserChildComponentBaseComponent extends CommonChildComponentBaseComponent {
+
+  constructor(public override authService:AuthService) {super(authService) }
+
+  loggedIn(){
+    return this.authService.loggedIn()
+  }
+
+}
