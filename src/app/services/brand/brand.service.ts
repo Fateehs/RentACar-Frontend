0cbab249b3cf4e192d183228d922d9532b0ba409
Fateehs <benzer1010@gmail.com>
2022-11-06 10:09:01 +0300

    CRUD Operations added

--- src/app/services/brand/brand.service.ts
+++ src/app/services/brand/brand.service.ts
@@ -1,15 +1,57 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
-import { GETBRANDS_URL } from 'src/app/models/constants/urls';
+import {
+  BRANDADD_URL,
+  BRANDDELETE_URL,
+  BRANDUPDATE_URL,
+  GETBRANDS_URL,
+} from 'src/app/models/constants/urls';
 import { Brand } from 'src/app/models/entitymodels/brand';
 import { ListResponseModel } from 'src/app/models/responsemodels/listResponseModel';
+import { ResponseModel } from 'src/app/models/responsemodels/responseModel';
+import { TemplatesService } from '../template/templates.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BrandService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private toastrService: ToastrService,
+    private templatesService: TemplatesService
+  ) {}
+
+  add(brand: Brand) {
+    this.httpClient.post<ResponseModel>(BRANDADD_URL, brand).subscribe(
+      (response) => {
+        this.toastrService.success(response.message);
+        window.location.reload();
+      },
+      (errorResponse) => this.templatesService.errorResponse(errorResponse)
+    );
+  }
+
+  update(brand: Brand) {
+    this.httpClient.post<ResponseModel>(BRANDUPDATE_URL, brand).subscribe(
+      (response) => {
+        this.toastrService.success(response.message);
+        window.location.reload();
+      },
+      (errorResponse) => this.templatesService.errorResponse(errorResponse)
+    );
+  }
+
+  delete(brand: Brand) {
+    this.httpClient.post<ResponseModel>(BRANDDELETE_URL, brand).subscribe(
+      (response) => {
+        this.toastrService.success(response.message);
+        window.location.reload();
+      },
+      (errorResponse) => this.templatesService.errorResponse(errorResponse)
+    );
+  }
 
   getAll(): Observable<ListResponseModel<Brand>> {
     return this.httpClient.get<ListResponseModel<Brand>>(GETBRANDS_URL);
