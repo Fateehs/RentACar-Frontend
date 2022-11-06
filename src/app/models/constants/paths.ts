d710bf9b366a54d6e8b5d4034416cf5b0ccf3086
Fateehs <benzer1010@gmail.com>
2022-11-06 10:07:14 +0300

    CRUD Operations Paths

--- src/app/models/constants/paths.ts
+++ src/app/models/constants/paths.ts
@@ -1,15 +1,27 @@
 // HOMEPAGE PATH
-export const HomePagePath = ''
+export const HomePagePath = '';
 
 // CAR PATHS
-export const CarDetailsPath = 'car-details'
-export const CarDetailsByBrandIdPath = 'car-details/brand/'
-export const CarDetailsByColorIdPath = 'car-details/color/'
-export const CarDetailByIdPath = 'car-detail/'
+export const CarDetailsPath = 'car-details';
+export const CarDetailsByBrandIdPath = 'car-details/brand/';
+export const CarDetailsByColorIdPath = 'car-details/color/';
+export const CarDetailByIdPath = 'car-detail/';
+export const CarAddPath = 'cars/add';
+export const CarUpdatePath = 'cars/update';
+export const CarDeletePath = 'cars/delete';
+
+// COLOR PATHS
+export const ColorAddPath = 'colors/add';
+export const ColorUpdatePath = 'colors/update';
+export const ColorDeletePath = 'colors/delete';
+
+// BRAND PATHS
+export const BrandAddPath = 'brands/add';
+export const BrandUpdatePath = 'brands/update';
+export const BrandDeletePath = 'brands/delete';
 
 // RENT PATHS
-export const RentByCarIdPath = 'rent/'
+export const RentByCarIdPath = 'rent/';
 
 // PAYMENT PATHS
-export const PayPath = 'pay'
-
+export const PayPath = 'pay';
