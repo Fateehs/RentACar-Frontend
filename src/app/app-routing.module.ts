28b7cfc37234a8ca9cef2138d809e43d3f78810c
Fateehs <benzer1010@gmail.com>
2022-11-06 10:08:21 +0300

    ADD Operation Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddCarComponent } from './components/admin/pages/adds/car-add/add-car.component';
+import { EntityManagementRouterButtonsPageComponent } from './components/admin/layouts/router-buttons/entity-management-router-buttons-page/entity-management-router-buttons-page.component';
+import { AddCarComponent } from './components/admin/pages/management/adds/add-car/add-car.component';
+import { DelCarComponent } from './components/admin/pages/management/deletes/del-car/del-car.component';
+import { UpdCarComponent } from './components/admin/pages/management/updates/upd-car/upd-car.component';
 import { CarDetailComponent } from './components/public/pages/cardetail/cardetail.component';
 import { CarDetailsComponent } from './components/public/pages/cardetails/cardetails.component';
 import { LoginComponent } from './components/public/pages/login/login.component';
@@ -18,10 +21,17 @@ const routes: Routes = [
   { path: 'car-details/color/:colorId', component: CarDetailsComponent },
   { path: CarDetailByIdPath + ':' + CarId, component: CarDetailComponent },
   { path: RentByCarIdPath + ':' + CarId, component: RentComponent },
+  { path: 'cars/add', component: AddCarComponent },
+  { path: 'cars/update', component: UpdCarComponent },
+  { path: 'cars/delete', component: DelCarComponent },
   { path: 'pay', component: PayComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cars/add', component: AddCarComponent, canActivate: [LoginGuard] },
+  {
+    path: 'entity-management-page',
+    component: EntityManagementRouterButtonsPageComponent,
+    canActivate: [LoginGuard],
+  },
 ];
 
 @NgModule({
