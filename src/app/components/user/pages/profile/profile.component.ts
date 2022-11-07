d23c322d927406dfc2812fbec14fd390c1a422ab
Fateehs <benzer1010@gmail.com>
2022-11-07 14:16:40 +0300

    User Profile added

---
+++ src/app/components/user/pages/profile/profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/app/models/entitymodels/user-dto';
+import { UserService } from 'src/app/services/user/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+})
+export class ProfileComponent implements OnInit {
+  currentUserDTO: UserDTO;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getCurrentUserDTO();
+  }
+
+  getCurrentUserDTO() {
+    this.userService.getCurrentUserDTO().subscribe((response) => {
+      this.currentUserDTO = response.data;
+    });
+  }
+}
