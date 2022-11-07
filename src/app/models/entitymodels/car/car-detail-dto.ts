aaceac33a28e462b4c3629dcb18bbb94e1393a7d
Fateehs <benzer1010@gmail.com>
2022-11-07 14:18:39 +0300

    Findeks and Description added

--- src/app/models/entitymodels/car/car-detail-dto.ts
+++ src/app/models/entitymodels/car/car-detail-dto.ts
@@ -1,13 +1,15 @@
-import { CarImage } from "./carImage";
+import { CarImage } from './carImage';
 
 export interface CarDetailDTO {
-    carId: number;
-    brandId: number;
-    colorId: number;
-    imagePath: CarImage[];
-    carName: string;
-    brandName: string;
-    colorName: string;
-    modelYear?: Date;
-    dailyPrice: number;
-  }
+  carId: number;
+  brandId: number;
+  colorId: number;
+  imagePath: CarImage[];
+  carName: string;
+  brandName: string;
+  colorName: string;
+  modelYear?: Date;
+  dailyPrice: number;
+  description: string;
+  findeksPoint: number;
+}
