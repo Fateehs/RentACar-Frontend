46016ba2da03b59a5f8185c3efcdf119c16662df
Fateehs <benzer1010@gmail.com>
2022-11-07 14:16:00 +0300

    Profile Page Button added

---
+++ src/app/components/user/layouts/router-buttons/route-to-profile-page-button/route-to-profile-page-button.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { RouterService } from 'src/app/services/router/router.service';
+import { UserChildComponentBaseComponent } from '../../../bases/user-child-component-base/user-child-component-base.component';
+
+@Component({
+  selector: 'app-route-to-profile-page-button',
+  templateUrl: './route-to-profile-page-button.component.html',
+  styleUrls: ['./route-to-profile-page-button.component.css'],
+})
+export class RouteToProfilePageButtonComponent
+  extends UserChildComponentBaseComponent
+  implements OnInit
+{
+  constructor(
+    private routerService: RouterService,
+    public override authService: AuthService
+  ) {
+    super(authService);
+    this.innerHTML = 'Profile';
+  }
+
+  ngOnInit(): void {}
+
+  routeToProfilePage() {
+    this.routerService.profilePage();
+  }
+}
