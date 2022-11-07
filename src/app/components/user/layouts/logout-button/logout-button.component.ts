703eb5504bb00424c3cd710ddef9a9d68ed60326
Fateehs <benzer1010@gmail.com>
2022-11-07 14:15:41 +0300

    Logout button added

---
+++ src/app/components/user/layouts/logout-button/logout-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserChildComponentBaseComponent } from '../../bases/user-child-component-base/user-child-component-base.component';
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  styleUrls: ['./logout-button.component.css'],
+})
+export class LogoutButtonComponent
+  extends UserChildComponentBaseComponent
+  implements OnInit
+{
+  constructor(public override authService: AuthService) {
+    super(authService);
+    this.innerHTML = 'Logout';
+  }
+
+  ngOnInit(): void {}
+
+  logout() {
+    this.authService.logout();
+  }
+}
