22976f14f3285d9156c789a9cd3d6ad1adbbdf23
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-13 20:02:42 +0300

    Router Paths added

--- src/app/models/constants/paths.ts
+++ src/app/models/constants/paths.ts
@@ -9,19 +9,29 @@ export const CarDetailByIdPath = 'car-detail/';
 export const CarAddPath = 'cars/add';
 export const CarUpdatePath = 'cars/update';
 export const CarDeletePath = 'cars/delete';
+export const CarDetailListPath = 'car-detail-list';
+export const AddCarPath = 'add-car'
 
 // COLOR PATHS
 export const ColorAddPath = 'colors/add';
 export const ColorUpdatePath = 'colors/update';
 export const ColorDeletePath = 'colors/delete';
+export const ColorListPath = 'color-list';
+export const AddColorPath = 'add-color'
 
 // BRAND PATHS
 export const BrandAddPath = 'brands/add';
 export const BrandUpdatePath = 'brands/update';
 export const BrandDeletePath = 'brands/delete';
+export const BrandListPath = 'brand-list';
+export const AddBrandPath = 'add-brand';
 
 // RENT PATHS
 export const RentByCarIdPath = 'rent/';
+export const RentalDetailListPath = 'rental-detail-list';
+
+// CUSTOMER PATHS
+export const CustomerListPath = 'color-list';
 
 // PAYMENT PATHS
 export const PayPath = 'pay';
@@ -35,3 +45,6 @@ export const ProfilePath = 'profile';
 
 // ENTITY MANAGEMENT PATHS
 export const EntityManagementPath = 'entity-management-page';
+
+// CAR IMAGE PATHS
+export const AddCarImageByCarIdPath = 'add-car-image/'
