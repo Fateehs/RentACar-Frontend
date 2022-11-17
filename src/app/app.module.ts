3212f602835a970e53bf3dad32537e0e8eee4795
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-17 17:49:26 +0300

    Administration Components added for module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminChildComponentBaseComponent } from './components/admin/bases/admin-child-component-base/admin-child-component-base.component';
 import { AdministrationDropdownComponent } from './components/admin/layouts/administration-dropdown/administration-dropdown.component';
+import { DeleteBrandComponent } from './components/admin/layouts/deletes/delete-brand/delete-brand.component';
+import { DeleteCarImageComponent } from './components/admin/layouts/deletes/delete-car-image/delete-car-image.component';
+import { DeleteCarComponent } from './components/admin/layouts/deletes/delete-car/delete-car.component';
+import { DeleteColorComponent } from './components/admin/layouts/deletes/delete-color/delete-color.component';
 import { RouteToAddBrandPageButtonComponent } from './components/admin/layouts/router-buttons/route-to-add-brand-page-button/route-to-add-brand-page-button.component';
 import { RouteToAddCarImagePageButtonComponent } from './components/admin/layouts/router-buttons/route-to-add-car-image-page-button/route-to-add-car-image-page-button.component';
 import { RouteToAddCarPageButtonComponent } from './components/admin/layouts/router-buttons/route-to-add-car-page-button/route-to-add-car-page-button.component';
@@ -18,15 +22,17 @@ import { RouteToCarDetailListPageButtonComponent } from './components/admin/layo
 import { RouteToColorListPageButtonComponent } from './components/admin/layouts/router-buttons/route-to-color-list-page-button/route-to-color-list-page-button.component';
 import { RouteToCustomerListPageButtonComponent } from './components/admin/layouts/router-buttons/route-to-customer-list-page-button/route-to-customer-list-page-button.component';
 import { RouteToRentalDetailListPageButtonComponent } from './components/admin/layouts/router-buttons/route-to-rental-detail-list-page-button/route-to-rental-detail-list-page-button.component';
+import { UpdateBrandComponent } from './components/admin/layouts/updates/update-brand/update-brand.component';
+import { UpdateCarComponent } from './components/admin/layouts/updates/update-car/update-car.component';
+import { UpdateColorComponent } from './components/admin/layouts/updates/update-color/update-color.component';
+import { BrandListComponent } from './components/admin/pages/lists/brand-list/brand-list.component';
+import { CarDetailListComponent } from './components/admin/pages/lists/car-detail-list/car-detail-list.component';
+import { ColorListComponent } from './components/admin/pages/lists/color-list/color-list.component';
+import { CustomerListComponent } from './components/admin/pages/lists/customer-list/customer-list.component';
+import { RentalDetailListComponent } from './components/admin/pages/lists/rental-detail-list/rental-detail-list.component';
 import { AddBrandComponent } from './components/admin/pages/management/adds/add-brand/add-brand.component';
 import { AddCarComponent } from './components/admin/pages/management/adds/add-car/add-car.component';
 import { AddColorComponent } from './components/admin/pages/management/adds/add-color/add-color.component';
-import { DelBrandComponent } from './components/admin/pages/management/deletes/del-brand/del-brand.component';
-import { DelCarComponent } from './components/admin/pages/management/deletes/del-car/del-car.component';
-import { DelColorComponent } from './components/admin/pages/management/deletes/del-color/del-color.component';
-import { UpdBrandComponent } from './components/admin/pages/management/updates/upd-brand/upd-brand.component';
-import { UpdCarComponent } from './components/admin/pages/management/updates/upd-car/upd-car.component';
-import { UpdColorComponent } from './components/admin/pages/management/updates/upd-color/upd-color.component';
 import { CommonChildComponentBaseComponent } from './components/public/bases/common-child-component-base/common-child-component-base.component';
 import { PublicChildComponentBaseComponent } from './components/public/bases/public-child-component-base/public-child-component-base.component';
 import { BrandAndColorSelectOptionCarFilterComponent } from './components/public/layouts/brand-and-color-select-option-car-filter/brand-and-color-select-option-car-filter.component';
@@ -36,6 +42,7 @@ import { ColorSelectOptionCarFilterComponent } from './components/public/layouts
 import { NaviComponent } from './components/public/layouts/navi/navi.component';
 import { RouteToCarDetailPageButtonComponent } from './components/public/layouts/router-buttons/route-to-car-detail-page-button/route-to-car-detail-page-button.component';
 import { RouteToCarDetailsPageButtonComponent } from './components/public/layouts/router-buttons/route-to-car-details-page-button/route-to-car-details-page-button.component';
+import { RouteToHomePageButtonComponent } from './components/public/layouts/router-buttons/route-to-home-page-button/route-to-home-page-button.component';
 import { RouteToLoginPageButtonComponent } from './components/public/layouts/router-buttons/route-to-login-page-button/route-to-login-page-button.component';
 import { RouteToRegisterPageButtonComponent } from './components/public/layouts/router-buttons/route-to-register-page-button/route-to-register-page-button.component';
 import { RouteToRentPageButtonComponent } from './components/public/layouts/router-buttons/route-to-rent-page-button/route-to-rent-page-button.component';
@@ -61,7 +68,6 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { BrandsPipe } from './pipes/brands-filter.pipe';
 import { CarDetailsFilterPipe } from './pipes/car-details-filter.pipe';
 import { ColorsPipe } from './pipes/colors-filter.pipe';
-import { RouteToHomePageButtonComponent } from './components/public/layouts/router-buttons/route-to-home-page-button/route-to-home-page-button.component';
 
 @NgModule({
   declarations: [
@@ -90,15 +96,9 @@ import { RouteToHomePageButtonComponent } from './components/public/layouts/rout
     SmallNumberInputComponent,
     DeletePaymentComponent,
     UserChildComponentBaseComponent,
-    UpdCarComponent,
     AdminChildComponentBaseComponent,
-    DelCarComponent,
     AddBrandComponent,
     AddColorComponent,
-    DelBrandComponent,
-    DelColorComponent,
-    UpdBrandComponent,
-    UpdColorComponent,
     RouteToLoginPageButtonComponent,
     RouteToRegisterPageButtonComponent,
     RouteToProfilePageButtonComponent,
@@ -120,6 +120,19 @@ import { RouteToHomePageButtonComponent } from './components/public/layouts/rout
     RouteToCustomerListPageButtonComponent,
     RouteToRentalDetailListPageButtonComponent,
     RouteToHomePageButtonComponent,
+    CarDetailListComponent,
+    BrandListComponent,
+    ColorListComponent,
+    CustomerListComponent,
+    RentalDetailListComponent,
+    
+    DeleteBrandComponent,
+    DeleteCarImageComponent,
+    DeleteCarComponent,
+    DeleteColorComponent,
+    UpdateBrandComponent,
+    UpdateCarComponent,
+    UpdateColorComponent,
   ],
   imports: [
     BrowserModule,
