ed682ed6bdd8f1fe264d1afc5281d5f7fcb4dc11
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-20 12:11:27 +0300

    Added Messages for Magic String Issues

--- src/app/models/constants/messages.ts
+++ src/app/models/constants/messages.ts
@@ -1,1 +1,3 @@
 export const AbsentForm = 'Form Has Absent Values Be Carefull!'
+export const PleaseLogin = 'Please login to continue'
+export const PermissionError = 'You do not have an permission to view this page!'
